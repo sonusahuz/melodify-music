1e399f4fe1cf8720f1d89d55d9636d951714a454
sonusahuz <sonusahu8930@gmail.com>
2024-07-29 07:31:58 +0530

    fix:some bugs

--- src/app/(pages)/library/liked-songs/page.tsx
+++ src/app/(pages)/library/liked-songs/page.tsx
@@ -15,12 +15,12 @@ const LikedSongs = () => {
   }
 
   return (
-    <div className='mb-36'>
-      <div className='flex items-center justify-between'>
-        <h1 className='text-2xl font-bold'>Liked Songs</h1>
-        <span  className='text-sm'>{favorites.length} Songs</span>
+    <div className="mb-36">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Liked Songs</h1>
+        <span className="text-sm">{favorites.length} Songs</span>
       </div>
-      <div className="flex items-center justify-start flex-wrap  mx-auto">
+      <div className="flex items-center justify-start flex-wrap gap-3 mx-auto mt-4">
         {favorites.map((song) => (
           <SongList key={song.id} song={song} />
         ))}
